2f9c05386713099d994d28bea19c1c0bb5d9fff6
raulpy271 <raulrufino0@gmail.com>
2024-03-13 21:03:04 -0300

    Implement lexer

--- src/Molecules.hs
+++ src/Molecules.hs
@@ -1,5 +1,8 @@
 module Molecules where
 
+import Data.Char
+
+
 data TokenType = Molecule | Number Int | OBrackets | CBrackets | OSquareBrackets | CSquareBrackets | OCurlyBrackets | CCurlyBrackets 
   deriving (Eq, Ord, Read, Show)
   
@@ -16,4 +19,28 @@ lexer ('[':s) = Token { tokenType=OSquareBrackets, tokenStr="[" } : lexer s
 lexer (']':s) = Token { tokenType=CSquareBrackets, tokenStr="]" } : lexer s
 lexer ('{':s) = Token { tokenType=OCurlyBrackets, tokenStr="{" } : lexer s
 lexer ('}':s) = Token { tokenType=CCurlyBrackets, tokenStr="}" } : lexer s
-lexer (c:s) =   Token { tokenType=Number 0, tokenStr="(" }  : lexer s
+
+lexer str =
+  let (c:s) = str in
+    if isAlphaNum c then
+      if isDigit c then
+        let (digits, rest) = splitNumber str in
+          Token { tokenType=Number (read digits), tokenStr=digits } : lexer rest
+      else 
+        if isUpper c then
+          let (lower, rest) = splitLower s in
+            Token { tokenType=Molecule, tokenStr=c:lower } : lexer rest
+        else lexer s
+    else lexer s
+
+splitNumber = splitWhile isDigit
+
+splitLower = splitWhile (\c -> isAlpha c && isLower c)
+
+splitWhile :: (Char -> Bool) -> String -> (String, String)
+splitWhile _ [] = ("", "")
+splitWhile f (c:s) = if f c then
+  let (n, r) = splitWhile f s in
+    (c:n, r)
+  else ("", c:s)
+
--- test/Main.hs
+++ test/Main.hs
@@ -1,17 +1,48 @@
 module Main (main) where
 
 
-import Test.HUnit
+import Test.Hspec
+import Control.Exception (evaluate)
 import Molecules
 
-test1 = TestCase (assertEqual "Should parse Open brackets" 
-  [Token {tokenType=OBrackets, tokenStr="("}]
-  (lexer "("))
-
-tests = TestList [TestLabel "test1" test1]
-
 main :: IO ()
---main = putStrLn "Test suite not yet implemented."
-main = do
-  _ <- runTestTT tests
-  putStrLn "Tests finished"
+main = hspec $ do
+  describe "brackets" $ do
+    it "brackets" $ do
+      lexer "()" `shouldBe` [Token {tokenType=OBrackets, tokenStr="("}, Token {tokenType=CBrackets, tokenStr=")"}]
+    it "square brackets" $ do
+      lexer "[]" `shouldBe` [Token {tokenType=OSquareBrackets, tokenStr="["}, Token {tokenType=CSquareBrackets, tokenStr="]"}]
+
+  describe "numbers" $ do
+    it "One digit" $ do
+      lexer "1" `shouldBe` [Token {tokenType=Number 1, tokenStr="1"}]
+    it "Multiple digits" $ do
+      lexer "12" `shouldBe` [Token {tokenType=Number 12, tokenStr="12"}]
+
+  describe "molecule name" $ do
+    it "Single letter" $ do
+      lexer "H" `shouldBe` [Token {tokenType=Molecule, tokenStr="H"}]
+    it "Multiple letters" $ do
+      lexer "He" `shouldBe` [Token {tokenType=Molecule, tokenStr="He"}]
+    it "wrong name" $ do
+      lexer "h" `shouldBe` []
+
+  describe "Multiple molecules" $ do
+    it "Single letter" $ do
+      lexer "HF" `shouldBe` [Token {tokenType=Molecule, tokenStr="H"}, Token {tokenType=Molecule, tokenStr="F"}]
+    it "Two letters" $ do
+      lexer "HeFFa" `shouldBe` [
+        Token {tokenType=Molecule, tokenStr="He"},
+        Token {tokenType=Molecule, tokenStr="F"},
+        Token {tokenType=Molecule, tokenStr="Fa"}]
+
+  describe "splitNumber" $ do
+    it "One digit" $ do
+      splitNumber "1" `shouldBe` ("1", "")
+    it "Two digit" $ do
+      splitNumber "12" `shouldBe` ("12", "")
+    it "Two digit with remaining" $ do
+      splitNumber "12s2" `shouldBe` ("12", "s2")
+    it "No digits" $ do
+      splitNumber "s12" `shouldBe` ("", "s12")
+
